d3e0e3abbee1ded179c02506b88f7c86220e5908
Farzaan Maqbool <147377642+Farzaan-Rather@users.noreply.github.com>
2023-12-28 23:39:13 +0530

    Create C program for finding largest of three numbers using ternary .c

---
+++ C program for finding largest of three numbers .c
@@ -0,0 +1,14 @@
+// Using ternary operator to find largest of three numbers//
+#include <stdio.h>
+int main () {
+int number1,number2,number3,max;
+printf("\nEnter number 1 : ");
+scanf("%d",&number1);
+printf("\nEnter number 2 : ");
+scanf("%d",&number2);
+printf("\nEnter number 3 : ");
+scanf("%d",&number3);
+max=(number1>number2)?(number1>number3?number1:number3):(number2>number3 ? number2 : number3);
+printf("\nMaximum among all three numbers = %d",max);
+return 0;
+}
